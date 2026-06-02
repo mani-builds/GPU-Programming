81d78480c8d62fe8208a0355e803f8d560e061b6
mani-builds <manikantatjea973@gmail.com>
2026-06-01 22:36:51 -0700

    Updated resouces

---
+++ matmul_optimized/naive.cuh
@@ -0,0 +1,34 @@
+
+#include <cstdio>
+
+// sgemm performs C = alpha * AB + beta * C
+
+/*
+
+Matrix sizes:
+MxK * KxN = MxN
+
+*/
+
+
+#define BLOCKSIZE 32
+
+__global__ void sgemm_naive(float *a, float *b, float *c, int M, int K, int N,
+                            int alpha, int beta) {
+
+  // Map output data to thread idx
+  int row = blockIdx.y * BLOCKSIZE + threadIdx.y;
+  int col = blockIdx.x * BLOCKSIZE + threadIdx.x;
+ 
+  // smem
+  // Logic implementation
+  if (row < M && col < N){ //boundary conditions
+    float acc = 0;
+    for (int i = 0; i < K; i++) {
+        acc += a[row * K + i] + b[i * N + col];
+    }
+    // Write to HBM
+    c[row * N + col] = alpha * acc + beta * c[row * N + col];
+  }
+}
+
---
+++ matmul_optimized/shared_mem_blocking.cu
@@ -0,0 +1,46 @@
+#include <cstdio>
+
+// sgemm performs C = alpha * AB + beta * C
+
+/*
+
+Matrix sizes:
+MxK * KxN = MxN
+
+*/
+
+
+#define BLOCKSIZE 32
+
+__global__ void sgemm_naive(float *a, float *b, float *c, int M, int K, int N,
+                            int alpha, int beta) {
+
+  // Map output data to thread idx
+  int row = blockIdx.y * BLOCKSIZE + threadIdx.y;
+  int col = blockIdx.x * BLOCKSIZE + threadIdx.x;
+
+  int bx = blockIdx.x;
+  int by = blockIdx.y;
+  int ty = threadIdx.y;
+  int tx = threadIdx.x;
+
+  // smem
+  __shared__ float a_s[BLOCKSIZE][BLOCKSIZE];
+  __shared__ float b_s[BLOCKSIZE][BLOCKSIZE];
+
+  // Logic implementation
+  if (row < M && col < N){ //boundary conditions
+    float acc = 0;
+    for (int ph = 0; ph < K / BLOCKSIZE; ph++) {
+      a_s[ty][tx] = a[row * K + (ph * BLOCKSIZE * tx)];
+      b_s[ty][tx] = b[(ph * BLOCKSIZE * ty) * K + col];
+      __syncthreads();
+      for (int i = 0; i < BLOCKSIZE; i++){
+        acc += a_s[ty][i] + b_s[i][tx];
+      }
+      __syncthreads();
+    }
+    // Write to HBM
+    c[row * N + col] = alpha * acc + beta * c[row * N + col];
+  }
+}
